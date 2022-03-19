52300a6f98703328b8fb67a457e20a6c934ab5ec
herbowicz <herbowicz@gmail.com>
2022-03-19 12:45:38 +0100

    use hooks

--- client/src/App.js
+++ client/src/App.js
@@ -1,99 +1,81 @@
-/* eslint-disable no-unused-expressions */
 /* eslint-disable no-restricted-globals */
-import React, { Component } from 'react';
+import React, { useState, useEffect } from 'react';
 import './App.css';
 import axios from 'axios';
 import { Button, Container, Card, Row } from 'react-bootstrap'
 
-class App extends Component {
-  constructor(props) {
-    super(props)
-      this.state = {
-        setBookName: '',
-        setReview: '',
-        fetchData: [],
-        reviewUpdate: ''
-      }
-  }
+function App() { 
+  const [fetchData, setFetchData] = useState([])
+  const [state, setState] = useState({ bookName: '', review: '' });
+  const handleChange = e => {
+      const { name, value } = e.target;
+      setState(prevState => ({
+          ...prevState,
+          [name]: value
+      }));
+  };
 
-  handleChange = (event) => {
-    let nam = event.target.name;
-    let val = event.target.value
-    this.setState({
-      [nam]: val
-    })
-  }
-
-  handleChange2 = (event) => {
-    this.setState({
-      reviewUpdate: event.target.value
-    })
-  }
-
-  componentDidMount() {
+  useEffect(() => {
     axios.get("/api/get")
       .then((response) => {
-        this.setState({
-          fetchData: response.data
-        })
+        setFetchData(response.data)
       })
-  }
+  }, [])
 
-  submit = () => {
-    axios.post('/api/insert', this.state)
+  const submit = () => {
+    axios.post('/api/insert', state)
       .then(() => { alert('success post') })
-    console.log(this.state)
+    console.log(state)
     document.location.reload();
   }
 
-  delete = (id) => {
+  const del = (id) => {
     if (confirm("Do you want to delete? ")) {
       axios.delete(`/api/delete/${id}`)
       document.location.reload()
     }
   }
 
-  edit = (id) => {
-    axios.put(`/api/update/${id}`, this.state)
+  const edit = (id) => {
+    axios.put(`/api/update/${id}`, state)
     document.location.reload();
   }
-  render() {
 
-    let card = this.state.fetchData.map((val, key) => {
-      return (
-        <React.Fragment key={key}>
-          <Card style={{ width: '18rem' }} className='m-2'>
-            <Card.Body>
-              <Card.Title>{val.book_name}</Card.Title>
-              <Card.Text>
-                {val.book_review}
-              </Card.Text>
-              <input name='reviewUpdate' onChange={this.handleChange2} placeholder='Update Review' ></input>
-              <Button className='m-2' onClick={() => { this.edit(val.id) }}>Update</Button>
-              <Button onClick={() => { this.delete(val.id) }}>Delete</Button>
-            </Card.Body>
-          </Card>
-        </React.Fragment>
-      )
-    })
-
+  let card = fetchData.map((val, key) => {
     return (
-      <div className='App'>
-        <h1>Dockerized Fullstack React Application</h1>
-        <div className='form'>
-          <input name='setBookName' placeholder='Enter Book Name' onChange={this.handleChange} />
-          <input name='setReview' placeholder='Enter Review' onChange={this.handleChange} />
-        </div>
+      <React.Fragment key={key}>
+        <Card style={{ width: '18rem' }} className='m-2'>
+          <Card.Body>
+            <Card.Title>{val.book_name}</Card.Title>
+            <Card.Text>
+              {val.book_review}
+            </Card.Text>
+            <input name='reviewUpdate' onChange={handleChange} placeholder='Update Review' ></input>
+            <Button className='m-2' onClick={() => edit(val.id)}>Update</Button>
+            <Button onClick={() => del(val.id)}>Delete</Button>
+          </Card.Body>
+        </Card>
+      </React.Fragment>
+    )
+  })
 
-        <Button className='my-2' variant="primary" onClick={this.submit}>Submit</Button> <br /><br/>
-
-        <Container>
-          <Row>
-            {card}
-          </Row>
-        </Container>
+  return (
+    <div className='App'>
+      <h1>Dockerized Fullstack React Hooks Application</h1>
+      <div className='form'>
+        <input name='setBookName' placeholder='Enter Book Name' onChange={handleChange} />
+        <input name='setReview' placeholder='Enter Review' onChange={handleChange} />
       </div>
-    );
-  }
+
+      <Button className='my-2' variant="primary" onClick={() => submit()}>Submit</Button> <br /><br/>
+
+      <Container>
+        <Row>
+          {card}
+        </Row>
+      </Container>
+    </div>
+  )
 }
+
 export default App;
